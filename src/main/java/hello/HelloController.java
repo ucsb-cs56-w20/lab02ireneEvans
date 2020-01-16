350106b4fc67937bd11a2b6426cb1c2600159850
Irene Evans <irene_evans@umail.ucsb.edu>
2020-01-15 19:36:13 -0800

    creating webapp

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
+        @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+    String html = "<h1>Hello World!</h1>\n" + 
+                "<p> This web app is powered by \n" +
+                 "<a href='https://github.com/ucsb-cs56-w20/lab02-mygithubid'>this github repo</a></p>\n";
+        return html;
+
     }
-    
 }
